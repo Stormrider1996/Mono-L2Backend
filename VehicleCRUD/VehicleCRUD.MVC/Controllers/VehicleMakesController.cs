b0a29fc153b37aa7ee5940c7df0c9d8467dd2345
Stormrider1996 <franjo.jumic@gmail.com>
2022-05-04 16:41:15 +0200

    Added controllers for VehicleMake and VehicleModel with coresponding views. Error with VehicleMake, asks for a paramterless ctor in the controller and  if I add it, it overwrites my ctor for instantiating the VehicleService and the DBContext object (throws NullReferenceException).

---
+++ VehicleCRUD/VehicleCRUD.MVC/Controllers/VehicleMakesController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VehicleCRUD.Service;
+using System.Data.Entity.Infrastructure;
+
+namespace VehicleCRUD.MVC.Controllers
+{
+    public class VehicleMakesController : Controller
+    {
+        private readonly VehiclesDbEntities Context;
+        private readonly IVehicleService VehicleService;
+
+        public VehicleMakesController(VehiclesDbEntities context, IVehicleService vehicleService)
+        {
+            Context = context;
+            VehicleService = vehicleService;  
+        }
+        
+        // GET: VehicleMakes
+        public async Task<ActionResult> Index()
+        {
+            return View(await VehicleService.GetVehicleMakeListAsync());
+        }
+
+        // GET: VehicleMakes/Details/5
+        public async Task<ActionResult> Details(string name)
+        {
+            if (name == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = await VehicleService.GetVehicleMakeByNameAsync(name);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleMakes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Abrv")] VehicleMake vehicleMake)
+        {
+            if (ModelState.IsValid)
+            {
+               await VehicleService.InsertVehicleMakeAsync(vehicleMake);
+               return RedirectToAction(nameof(Index));
+            }
+
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = await Context.VehicleMakes.FindAsync(id);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // POST: VehicleMakes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(string name, string abrv, [Bind(Include = "Id,Name,Abrv")] VehicleMake vehicleMake)
+        {
+            if (name != vehicleMake.Name) 
+            {
+                return HttpNotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await VehicleService.UpdateVehicleMakeAsync(vehicleMake, name, abrv);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleMakeExists(vehicleMake.Name))
+                    {
+                        return HttpNotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+
+            }
+            return View(vehicleMake);
+        }
+
+        // GET: VehicleMakes/Delete/5
+        public async Task<ActionResult> Delete(string name)
+        {
+            if (name == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleMake vehicleMake = await VehicleService.GetVehicleMakeByNameAsync(name);
+            if (vehicleMake == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleMake);
+        }
+
+        // POST: VehicleMakes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string name)
+        {
+            await VehicleService.DeleteMakeByNameAsync(name);
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VehicleMakeExists(string name)
+        {
+            return Context.VehicleMakes.Any(e => e.Name == name);
+        }
+    }
+}
---
+++ VehicleCRUD/VehicleCRUD.MVC/Controllers/VehicleModelsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VehicleCRUD.Service;
+
+namespace VehicleCRUD.MVC.Controllers
+{
+    public class VehicleModelsController : Controller
+    {
+        private VehiclesDbEntities db = new VehiclesDbEntities();
+
+        // GET: VehicleModels
+        public async Task<ActionResult> Index()
+        {
+            var vehicleModels = db.VehicleModels.Include(v => v.VehicleMake);
+            return View(await vehicleModels.ToListAsync());
+        }
+
+        // GET: VehicleModels/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = await db.VehicleModels.FindAsync(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Create
+        public ActionResult Create()
+        {
+            ViewBag.MakeId = new SelectList(db.VehicleMakes, "Id", "Name");
+            return View();
+        }
+
+        // POST: VehicleModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,Abrv,MakeId")] VehicleModel vehicleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                vehicleModel.Id = Guid.NewGuid();
+                db.VehicleModels.Add(vehicleModel);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MakeId = new SelectList(db.VehicleMakes, "Id", "Name", vehicleModel.MakeId);
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = await db.VehicleModels.FindAsync(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MakeId = new SelectList(db.VehicleMakes, "Id", "Name", vehicleModel.MakeId);
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Abrv,MakeId")] VehicleModel vehicleModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(vehicleModel).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MakeId = new SelectList(db.VehicleMakes, "Id", "Name", vehicleModel.MakeId);
+            return View(vehicleModel);
+        }
+
+        // GET: VehicleModels/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VehicleModel vehicleModel = await db.VehicleModels.FindAsync(id);
+            if (vehicleModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vehicleModel);
+        }
+
+        // POST: VehicleModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            VehicleModel vehicleModel = await db.VehicleModels.FindAsync(id);
+            db.VehicleModels.Remove(vehicleModel);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
