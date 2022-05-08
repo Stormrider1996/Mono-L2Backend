729cf84525727ab0a183ecf1a4b4765b1dba9470
Stormrider1996 <franjo.jumic@gmail.com>
2022-05-08 22:49:51 +0200

    Added viewmodel for counting makes and models

--- VehicleCRUD/VehicleCRUD.MVC/Controllers/HomeController.cs
+++ VehicleCRUD/VehicleCRUD.MVC/Controllers/HomeController.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using VehicleCRUD.MVC.ViewModels;
+using VehicleCRUD.Service;
 
 namespace VehicleCRUD.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly IVehicleModelService VehicleModelService;
+        private readonly IVehicleMakeService VehicleMakeService;
+        public HomeController(IVehicleModelService vehicleModelService, IVehicleMakeService vehicleMakeService)
         {
-            return View();
+            VehicleModelService = vehicleModelService;
+            VehicleMakeService = vehicleMakeService;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var vehicleMakeList = await VehicleMakeService.GetVehicleMakeListAsync();
+            var vehicleModelList = await VehicleModelService.GetVehicleModelListAsync();
+            VehicleMakeAndModelCount count = new VehicleMakeAndModelCount()
+            {
+                VehicleMakeCount = vehicleMakeList.Count(),
+                VehicleModelCount = vehicleModelList.Count(),
+            };
+            IEnumerable<VehicleMakeAndModelCount> result = new List<VehicleMakeAndModelCount> { count };
+            return View(result);
         }
 
         public ActionResult About()
---
+++ VehicleCRUD/VehicleCRUD.MVC/ViewModels/VehicleMakeAndModelCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VehicleCRUD.MVC.ViewModels
+{
+    public class VehicleMakeAndModelCount
+    {
+        public int VehicleMakeCount { get; set; }
+        public int VehicleModelCount { get; set; }
+    }
+}
